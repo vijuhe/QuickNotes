00f9d2e8c3e26b72c6f7b2038844d99ef1b1d962
Ville Henriksson <ville.henriksson@vincit.fi>
2019-09-04 15:18:14 +0300

    Counts component logic and layout.

--- app/counts/counts.component.js
+++ app/counts/counts.component.js
@@ -5,5 +5,17 @@ angular.module('quickNotes')
   templateUrl: 'counts/counts.template.html',
   bindings: {
       notes: '<'
+  },
+  
+  controller: function($scope) {
+    this.$onInit = function() {
+        $scope.total = this.notes.length;
+        $scope.completed = this.getLength(this.notes, 'Completed');
+        $scope.notCompleted = this.getLength(this.notes, 'Not completed');
+    }
+
+    this.getLength = function(array, status) {
+        return array.filter(function(item) { return item.status === status; }).length;
+    }
   }
 });
