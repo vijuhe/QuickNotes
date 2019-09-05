05d191d2f360fa9b1ed0f37eddb3ad4a7efe9ccd
Ville Henriksson <ville.henriksson@vincit.fi>
2019-09-05 08:11:14 +0300

    Remove router.

--- app/app.js
+++ app/app.js
@@ -4,18 +4,8 @@ import NotesService from './services/notes.service.js';
 
 // Declare app level module which depends on views, and core components
 angular.module('quickNotes', [
-  'ngRoute',
   'ngResource' // not needed now with hard-coded data
 ])
-
-// .config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-//   $locationProvider.hashPrefix('!');
-//   $routeProvider.when('/', {
-//     templateUrl: 'mainView/mainView.html',
-//     controller: 'MainController'
-//   });
-
-// }])
 
 .factory('NotesService', function() {
   return new NotesService();
