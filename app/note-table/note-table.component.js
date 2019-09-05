f09082ed73c6f84ecc8466ea4dd788e8a7dae843
Ville Henriksson <ville.henriksson@vincit.fi>
2019-09-05 12:49:22 +0300

    Note table sorting by clicking columns.

--- app/note-table/note-table.component.js
+++ app/note-table/note-table.component.js
@@ -12,10 +12,24 @@ angular.module('quickNotes')
       $scope.selectAll = false;
       $scope.noteSelections = new Array(this.notes.length);
       $scope.notes = this.notes;
+      $scope.sort = {
+        column: "id",
+        descending: false
+      };
     }
 
     $scope.toggleSelectAll = function() {
       this.noteSelections.fill(this.selectAll);
+    }
+
+    $scope.changeSorting = function(column) {
+      if (this.sort.column === column) {
+        this.sort.descending = !this.sort.descending;
+      }
+      else {
+        this.sort.column = column;
+        this.sort.descending = false;
+      }
     }
   }
 });
