06b8eb0033a26984c2fd6ee0d11fe24abc535769
Ville Henriksson <ville.henriksson@vincit.fi>
2019-09-05 12:32:51 +0300

    Note table with styles.

--- app/note-table/note-table.component.js
+++ app/note-table/note-table.component.js
@@ -5,5 +5,17 @@ angular.module('quickNotes')
   templateUrl: 'note-table/note-table.template.html',
   bindings: {
     notes: '<'
-}
+  },
+
+  controller: function($scope) {
+    this.$onInit = function() {
+      $scope.selectAll = false;
+      $scope.noteSelections = new Array(this.notes.length);
+      $scope.notes = this.notes;
+    }
+
+    $scope.toggleSelectAll = function() {
+      this.noteSelections.fill(this.selectAll);
+    }
+  }
 });
