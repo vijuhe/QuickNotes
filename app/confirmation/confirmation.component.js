e28d72fbfa9c92a7db95cbd38d64749cddf32ffb
Ville Henriksson <ville.henriksson@vincit.fi>
2019-09-08 10:49:57 +0300

    Delete confirmation component layout and styling.

--- app/confirmation/confirmation.component.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('quickNotes')
-.component('confirmation', {
-  templateUrl: 'confirmation/confirmation.template.html'
-});
---
+++ app/delete-confirmation/delete-confirmation.component.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('quickNotes')
+.component('deleteConfirmation', {
+  templateUrl: 'delete-confirmation/delete-confirmation.template.html'
+});
